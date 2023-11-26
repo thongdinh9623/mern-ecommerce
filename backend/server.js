bf44ababa17b3bb280ba14bbc93ad18b7584b814
Đinh Đức Thông <ducthongdinh@yahoo.com>
2023-11-26 15:51:31 +0700

    separate endpoints to routes file.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
@@ -10,20 +11,6 @@ connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Response from API');
-});
-
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const productId = req.params.id;
-  console.log(productId);
-  const product = products.find((p) => p._id === productId);
-
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
