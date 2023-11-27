2fcb18c933c14f416b70dcf3205cdb3211526795
Đinh Đức Thông <ducthongdinh@yahoo.com>
2023-11-27 19:51:39 +0700

    Add error handling middleware.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
@@ -12,5 +13,7 @@ connectDB();
 const app = express();
 
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
