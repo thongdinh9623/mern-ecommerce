8e9df098d7477865c421074ef46aed4c33cca75c
Đinh Đức Thông <ducthongdinh@yahoo.com>
2023-12-31 21:26:04 +0700

    Add product API slice and Get products endpoint.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,45 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootswatch/dist/sketchy/bootstrap.css";
-import "./assets/index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/sketchy/bootstrap.css';
+import './assets/index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<HomeScreen />} />
-      <Route path="/product/:id" element={<ProductScreen />} />
+    <Route
+      path='/'
+      element={<App />}
+    >
+      <Route
+        index={true}
+        path='/'
+        element={<HomeScreen />}
+      />
+      <Route
+        path='/product/:id'
+        element={<ProductScreen />}
+      />
     </Route>
   )
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
