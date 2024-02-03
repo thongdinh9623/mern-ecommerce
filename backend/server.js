c095dce9af5319c801c964e72e3d19fee2b700ae
Đinh Đức Thông <ducthongdinh@yahoo.com>
2024-02-03 17:31:50 +0700

    Add build production command and serve build file on prod.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
@@ -13,6 +13,19 @@ connectDB();
 const app = express();
 
 app.use('/api/products', productRoutes);
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  // Any route that is not api will be redirected to index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
