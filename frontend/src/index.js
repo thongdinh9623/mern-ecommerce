29de71c67afdc502fdc903c2121091b2ac33c83f
Đinh Đức Thông <ducthongdinh@yahoo.com>
2024-08-15 23:44:52 +0700

    Add payment screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
 import ShippingScreen from './screens/ShippingScreen.jsx';
+import PaymentScreen from './screens/PaymentScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -53,6 +54,10 @@ const router = createBrowserRouter(
         <Route
           path='/shipping'
           element={<ShippingScreen />}
+        />
+        <Route
+          path='/payment'
+          element={<PaymentScreen />}
         />
       </Route>
     </Route>
