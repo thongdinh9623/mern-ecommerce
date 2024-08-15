cf83b60aee6c4f785b7bea78900bf1b2db5730e5
Đinh Đức Thông <ducthongdinh@yahoo.com>
2024-08-15 21:07:19 +0700

    Checkout Steps Component.

---
+++ frontend/src/screens/PaymentScreen.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Button, Col } from 'react-bootstrap';
+import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
+
+const PaymentScreen = () => {
+  return <div>PaymentScreen</div>;
+};
+
+export default PaymentScreen;
