aef240b32755914d937ee20327704b7f5ca80169
Đinh Đức Thông <ducthongdinh@yahoo.com>
2024-08-19 21:52:16 +0700

    Add orders route and controller

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 const __dirname = path.resolve();
 
